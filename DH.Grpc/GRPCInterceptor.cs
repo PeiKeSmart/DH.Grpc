93796fae3eebb20b3f72d93f7574a4974efe5e21
猿人易 <qcjxberin@outlook.com>
2025-01-05 11:34:45 +0800

    更新包版本并改进日志记录信息
    在 `DH.Grpc.csproj` 文件中，将 `Pek.AspNetCore` 包的版本从 `4.0.2024.1231-beta0926` 更新为 `4.0.2025.105-beta0316`。
    
    在 `GRPCInterceptor.cs` 文件中，修改了 `UnaryServerHandler` 方法的日志记录信息，增加了更详细的上下文信息，将原来的日志信息 `starting call` 和 `finished call` 分别改为 `[GRPCInterceptor.UnaryServerHandler]starting call` 和 `[GRPCInterceptor.UnaryServerHandler]finished call`，以便更清晰地标识日志来源。

--- DH.Grpc/GRPCInterceptor.cs
+++ DH.Grpc/GRPCInterceptor.cs
@@ -9,11 +9,11 @@ public class GRPCInterceptor : Interceptor
 {
     public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
     {
-        XTrace.Log.Debug($"starting call");
+        XTrace.Log.Debug($"[GRPCInterceptor.UnaryServerHandler]starting call");
 
         var response = await base.UnaryServerHandler(request, context, continuation);
 
-        XTrace.Log.Debug($"finished call");
+        XTrace.Log.Debug($"[GRPCInterceptor.UnaryServerHandler]finished call");
 
         return response;
     }
